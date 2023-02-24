54b8d135254e6aae9e05d6eb9ce4d4f74effb3e8
deprimiss <125207464+deprimiss@users.noreply.github.com>
2023-02-24 23:08:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@
 
 
 bool checkPrime(uint64_t value) {
-	// вставьте код функции
+// вставьте код функции
 for (int k = 2; k < sqrt(value); k++)
         if (value % k == 0)
             return false;
@@ -12,7 +12,7 @@ for (int k = 2; k < sqrt(value); k++)
 }
 
 uint64_t nPrime(uint64_t n) {
-	// вставьте код функции
+// вставьте код функции
 	int k = 2;
 	for (int i = 0;i < n;k++)
 		if (checkPrime(k))
@@ -21,12 +21,13 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-	// вставьте код функции
-for (int k = (value + 1);checkPrime(k) == 0;k++)
+// вставьте код функции
+	for (int k = (value + 1);checkPrime(k) == 0;k++)
 		return --k;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
+// вставьте код функции
 	int sum = 0;
 	for (int i = 2;i < hbound;i++)
 		if (checkPrime(i))

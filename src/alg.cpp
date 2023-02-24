c19b415fe66683d1e1572e514ecbd0d93b298052
deprimiss <125207464+deprimiss@users.noreply.github.com>
2023-02-25 00:25:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,30 +4,30 @@
 
 
 bool checkPrime(uint64_t value) {
-	for (int k = 2; k*k<value; k++)
+	for (uint64_t k = 2; k * k <= value; k++)
 		if (value % k == 0)
 			return false;
 	return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-	int k = 2;
-	for (int i = 0;i < n;k++)
+	uint64_t k = 2;
+	for (uint64_t i = 0;i < n;k++)
 		if (checkPrime(k))
 			i++;
 	return --k;
 }
 
 uint64_t nextPrime(uint64_t value) {
-	int k = (value + 1);
+	uint64_t k = (value + 1);
 	while (checkPrime(k) == 0)
 		k++;
 	return --k;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	int sum = 0;
-	for (int i = 2;i < hbound;i++)
+	uint64_t sum = 0;
+	for (uint64_t i = 2;i < hbound;i++)
 		if (checkPrime(i))
 			sum += i;
 	return sum;
